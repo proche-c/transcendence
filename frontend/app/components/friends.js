a8a421295ec6e682e00829f81489cec5f720a272
Paula Roche <paularoche@Mac-mini-de-Paula.local>
2025-05-12 14:48:24 +0200

    working on friends

--- frontend/app/components/friends.js
+++ frontend/app/components/friends.js
@@ -1,5 +1,4 @@
 "use strict";
-// tengo que crear la interfaz data!!!!!!!!!!!!!!!!!
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,10 +11,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 class FriendsComponent extends HTMLElement {
     constructor() {
         super();
-        this.response = null;
+        this.responseProfile = null;
+        this.responseFriends = null;
+        this.responseUsers = null;
+        this.user = null;
+        this.users = [];
+        this.friends = [];
         this.attachShadow({ mode: "open" });
-        this.getProfile();
-        this.render();
+        this.load();
+    }
+    load() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.getProfile();
+            yield this.getFriends();
+            yield this.getUsers();
+            this.render();
+            // this.updateData();
+        });
     }
     getProfile() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -26,8 +38,42 @@ class FriendsComponent extends HTMLElement {
                     credentials: "include",
                 });
                 const data = yield response.json();
-                this.response = data.user;
-                console.log(data.user);
+                this.user = data.user;
+                console.log(this.user);
+            }
+            catch (error) {
+                console.log('Error en la peticion');
+            }
+        });
+    }
+    getUsers() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield fetch("http://localhost:8000/users", {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                    credentials: "include",
+                });
+                const data = yield response.json();
+                this.users = data;
+                console.log(this.users);
+            }
+            catch (error) {
+                console.log('Error en la peticion');
+            }
+        });
+    }
+    getFriends() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield fetch("http://localhost:8000/users/friends", {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                    credentials: "include",
+                });
+                const data = yield response.json();
+                this.friends = data.friends;
+                console.log(this.friends);
             }
             catch (error) {
                 console.log('Error en la peticion');
@@ -40,14 +86,59 @@ class FriendsComponent extends HTMLElement {
         const style = document.createElement("link");
         style.rel = "stylesheet";
         style.href = "./app/tailwind.css"; // Asegúrate de que la ruta sea correcta
+        console.log("El user es :");
+        console.log(this.user);
+        const avatar = this.user.avatar;
+        const avatarUrl = `http://localhost:8000/static/${avatar}`;
+        const friendsButtons = this.friends.map((friend) => {
+            return `<button class="">${friend.username}</button>`;
+        }).join("");
+        const usersButtons = this.users.map((user) => {
+            if (user.username != this.user.username) {
+                const avatar = user.avatar;
+                const avatarUrl = `http://localhost:8000/static/${avatar}`;
+                return `
+					<div class="flex m-1">
+						<div class="w-8 h-8 rounded-full overflow-hidden border-4 border-black flex items-center justify-center bg-emerald-200">
+							<img src="${avatarUrl}" class="w-full h-full object-cover" />
+						</div>
+						<button class="ml-1">${user.username}</button>
+					</div>
+					`;
+            }
+            else
+                return null;
+        }).join("");
         this.shadowRoot.innerHTML = `
-			<div class="flex h-screen">
+			<div class="flex h-screen items-center">
 				<div>
 					<pong-menu></pong-menu>
 				</div>
-				<div>
-			<h1> Esto es friends </h1>
-			</div>
+				<div class="flex flex-col w-3/4 h-7/8">
+					<div class="flex flex-col items-center">
+						<div class="w-16 h-16 rounded-full overflow-hidden border-4 border-black flex items-center justify-center bg-emerald-200">
+							<img src="${avatarUrl}" class="w-full h-full object-cover" />
+						</div>
+						<div class="my-1">
+							<p>${this.user.username}</p>
+						</div>
+					</div>
+					<div class="flex grow ml-6 gap-4">
+						<div class=" bg-neutral-50 m-4 rounded-2xl flex flex-col flex-1 max-w-sm">
+							<h2 class="text-center border-b-1 border-violet-600 m-2 p-3">Friends</h2>
+							<div class="flex bg-neutral-50 flex-col flex-grow rounded-b-2xl">
+							${friendsButtons}
+							</div>
+						</div>
+						<div class=" bg-neutral-50 m-4 rounded-2xl flex flex-col flex-1 max-w-sm">
+							<h2 class="text-center border-b-1 border-violet-600 m-2 p-3">Users</h2>
+							<div class="flex bg-neutral-50 flex-col flex-grow rounded-b-2xl">
+							${usersButtons}
+							</div>
+						</div>
+
+					</div>
+				</div>
 			</div>
 		`;
         this.shadowRoot.appendChild(style);
--- frontend/app/components/friends.ts
+++ frontend/app/components/friends.ts
@@ -1,13 +1,30 @@
 // tengo que crear la interfaz data!!!!!!!!!!!!!!!!!
+interface User	{
+	id: number;
+	username: string;
+	avatar: string;
+}
 
 
 class FriendsComponent extends HTMLElement {
-	private response: any | null = null;
+	private responseProfile: any | null = null;
+	private responseFriends: any | null = null;
+	private responseUsers: any | null = null;
+	private user: User | any | null = null;
+	private users: Array<User> = [];
+	private friends: Array<User> = [];
 	constructor() {
 		super();
 		this.attachShadow({mode: "open"});
-		this.getProfile();
+		this.load();
+	}
+
+	private async load() {
+		await this.getProfile();
+		await this.getFriends();
+		await this.getUsers();
 		this.render();
+		// this.updateData();
 	}
 
 	private async getProfile() {
@@ -19,8 +36,44 @@ class FriendsComponent extends HTMLElement {
             });
 
 			const data = await response.json();
-			this.response = data.user;
-			console.log(data.user);
+			this.user = data.user;
+			console.log(this.user);
+
+
+		} catch (error: any) {
+			console.log('Error en la peticion');
+		}
+	}
+
+	private async getUsers() {
+		try {
+            const response = await fetch("http://localhost:8000/users", {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+            });
+
+			const data = await response.json();
+			this.users = data;
+			console.log(this.users);
+
+
+		} catch (error: any) {
+			console.log('Error en la peticion');
+		}
+	}
+
+	private async getFriends() {
+		try {
+            const response = await fetch("http://localhost:8000/users/friends", {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+            });
+
+			const data = await response.json();
+			this.friends = data.friends;
+			console.log(this.friends);
 
 
 		} catch (error: any) {
@@ -35,14 +88,62 @@ class FriendsComponent extends HTMLElement {
 		style.rel = "stylesheet";
 		style.href = "./app/tailwind.css"; // Asegúrate de que la ruta sea correcta
 
+		console.log("El user es :");
+		console.log(this.user);
+
+		const avatar = this.user.avatar;
+		const avatarUrl = `http://localhost:8000/static/${avatar}`;
+
+		const friendsButtons = this.friends.map((friend: User) => {
+			return `<button class="">${friend.username}</button>`;
+		}).join("");
+
+		const usersButtons = this.users.map((user: User) => {
+			if (user.username != this.user.username) {
+				const avatar = user.avatar;
+				const avatarUrl = `http://localhost:8000/static/${avatar}`;
+				return `
+					<div class="flex m-1">
+						<div class="w-8 h-8 rounded-full overflow-hidden border-4 border-black flex items-center justify-center bg-emerald-200">
+							<img src="${avatarUrl}" class="w-full h-full object-cover" />
+						</div>
+						<button class="ml-1">${user.username}</button>
+					</div>
+					`;
+			} else
+				return null;
+		}).join("");
+
 		this.shadowRoot.innerHTML = `
-			<div class="flex h-screen">
+			<div class="flex h-screen items-center">
 				<div>
 					<pong-menu></pong-menu>
 				</div>
-				<div>
-			<h1> Esto es friends </h1>
-			</div>
+				<div class="flex flex-col w-3/4 h-7/8">
+					<div class="flex flex-col items-center">
+						<div class="w-16 h-16 rounded-full overflow-hidden border-4 border-black flex items-center justify-center bg-emerald-200">
+							<img src="${avatarUrl}" class="w-full h-full object-cover" />
+						</div>
+						<div class="my-1">
+							<p>${this.user.username}</p>
+						</div>
+					</div>
+					<div class="flex grow ml-6 gap-4">
+						<div class=" bg-neutral-50 m-4 rounded-2xl flex flex-col flex-1 max-w-sm">
+							<h2 class="text-center border-b-1 border-violet-600 m-2 p-3">Friends</h2>
+							<div class="flex bg-neutral-50 flex-col flex-grow rounded-b-2xl">
+							${friendsButtons}
+							</div>
+						</div>
+						<div class=" bg-neutral-50 m-4 rounded-2xl flex flex-col flex-1 max-w-sm">
+							<h2 class="text-center border-b-1 border-violet-600 m-2 p-3">Users</h2>
+							<div class="flex bg-neutral-50 flex-col flex-grow rounded-b-2xl">
+							${usersButtons}
+							</div>
+						</div>
+
+					</div>
+				</div>
 			</div>
 		`;
 
