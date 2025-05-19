3aedb2d30c6c254eeff738505b2ec5151eb7e4bb
Paula Roche <paularoche@Mac-mini-de-Paula.local>
2025-05-19 12:32:58 +0200

    login and register changed

--- frontend/app/components/login.js
+++ frontend/app/components/login.js
@@ -18,6 +18,7 @@ class LoginComponent extends HTMLElement {
         this.inputData = null;
         this.errorMsg = null;
         this.response = null;
+        this.googleButton = null;
         this.attachShadow({ mode: "open" });
         this.render();
     }
@@ -28,44 +29,43 @@ class LoginComponent extends HTMLElement {
         style.rel = "stylesheet";
         style.href = "./app/tailwind.css";
         this.shadowRoot.innerHTML = `
-        <div class="relative py-3 sm:max-w-xl sm:mx-auto w-full">
-        <div class="relative px-4 py-10 bg-black mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-            <div class="max-w-md mx-auto text-white">
-                <div class="mt-5">
-                    <label for="email" class="font-semibold text-sm text-gray-400 pb-1 block">E-mail</label>
-                    <input id="email" type="text"
-                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
-                    <label for="password" class="font-semibold text-sm text-gray-400 pb-1 block">Password</label>
-                    <input id="password" type="password"
-                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
-                </div>
-                <div class="flex justify-center items-center">
-                    <div>
-                    <button class="flex items-center justify-center py-2 px-10 bg-white hover:bg-gray-200 focus:ring-blue-500 focus:ring-offset-blue-200 text-gray-700 w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
-                        <span class="ml-8">Sign in with Google</span>
-                    </button>
-                    </div>
-                </div>
-                <div class="mt-5">
-                    <button id="login"
-                        class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
-                        Log in
-                    </button>
-                </div>
-                <div class="flex items-center justify-between mt-4">
-                    <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-                    <button id="register" class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline">
-                        or sign up
-                    </button>
-                    <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-                </div>
-                <div class="text-red-600">
-                    <p id="error"> </p>
-                </div>
-            </div>
+<div class="max-w-md mx-auto text-white bg-black p-8 rounded-lg">
+    <form id="loginForm">
+        <div class="mt-1">
+            <label for="email" class="font-semibold text-sm text-gray-400 pb-1 block">E-mail</label>
+            <input id="email" type="text"
+                class="border rounded-lg px-3 py-2 mb-2 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
+            <label for="password" class="font-semibold text-sm text-gray-400 pb-1 block">Password</label>
+            <input id="password" type="password"
+                class="border rounded-lg px-3 py-2 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
         </div>
+        <div class="mt-5">
+            <button id="login" type="submit"
+                class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+                Log in
+            </button>
         </div>
-        `;
+    </form>
+    <div class="text-red-600 mt-1">
+      <p id="error" class="text-left text-sm"></p>
+    </div>
+
+    <hr class="border-gray-600 my-5" />
+
+    <div class="flex justify-center items-center">
+        <button id="google-login" class="mb-5 flex flex-row items-center justify-center py-2 px-4 bg-white hover:bg-gray-200 focus:ring-blue-500 focus:ring-offset-blue-200 text-gray-700 w-full transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg space-x-3">
+            <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google icon" class="w-5 h-5" />
+            <span>Sign in with Google</span>
+        </button>
+    </div>
+
+    <div>
+        <button id="register" class="flex items-center justify-center py-2 px-10 bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+            <span>Sign up</span>
+        </button>
+    </div>
+</div>
+`;
         this.shadowRoot.appendChild(style);
         this.emailInput = this.shadowRoot.querySelector("#email");
         this.passwordInput = this.shadowRoot.querySelector("#password");
@@ -73,11 +73,13 @@ class LoginComponent extends HTMLElement {
         this.inputData = this.shadowRoot.querySelector("#inputData");
         this.registerButton = this.shadowRoot.querySelector("#register");
         this.errorMsg = this.shadowRoot.querySelector("#error");
+        this.googleButton = this.shadowRoot.querySelector("#google-login");
         this.addEventListeners();
     }
     addEventListeners() {
         var _a, _b;
-        (_a = this.loginButton) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (event) => __awaiter(this, void 0, void 0, function* () {
+        const loginForm = (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.querySelector("#loginForm");
+        loginForm === null || loginForm === void 0 ? void 0 : loginForm.addEventListener("submit", (event) => __awaiter(this, void 0, void 0, function* () {
             var _a, _b;
             event.preventDefault();
             const email = ((_a = this.emailInput) === null || _a === void 0 ? void 0 : _a.value) || "";
@@ -93,12 +95,22 @@ class LoginComponent extends HTMLElement {
             console.log("he pulsado sign in");
             window.location.hash = "#register";
         });
+        if (this.googleButton) {
+            console.log("Google button found"); // Vérifiez si ce log s'affiche
+            this.googleButton.addEventListener("click", () => {
+                console.log("Google button clicked");
+                window.location.href = "http://localhost:8000/login/google";
+            });
+        }
+        else {
+            console.error("Google button not found");
+        }
     }
     postData(email, password) {
         return __awaiter(this, void 0, void 0, function* () {
             const data = { "email": email, "password": password };
             try {
-                // Esta url sera el endponit que configure el servidor
+                // Esta url sera el endpoint que configure el servidor
                 const response = yield fetch("http://localhost:8000/login", {
                     method: "POST",
                     body: JSON.stringify(data),
@@ -116,19 +128,13 @@ class LoginComponent extends HTMLElement {
                 }
                 if (response.status === 404 || response.status === 401) {
                     this.errorMsg.textContent = "Incorrect email or password";
-                    this.resetValues();
+                    //this.resetValues();
                 }
             }
             catch (error) {
                 console.log("error en la peticion");
             }
         });
-    }
-    resetValues() {
-        if (this.emailInput)
-            this.emailInput.value = "";
-        if (this.passwordInput)
-            this.passwordInput.value = "";
     }
 }
 customElements.define("pong-login", LoginComponent);
--- frontend/app/components/register.js
+++ frontend/app/components/register.js
@@ -16,8 +16,10 @@ class RegisterComponent extends HTMLElement {
         this.passwordInput = null;
         this.password2Input = null;
         this.registerButton = null;
+        this.registerForm = null;
         this.errorMsg = null;
         this.response = null;
+        this.debounceTimer = null; // Timer to not call the function too often
         this.attachShadow({ mode: "open" });
         this.render();
     }
@@ -28,75 +30,231 @@ class RegisterComponent extends HTMLElement {
         style.rel = "stylesheet";
         style.href = "./app/tailwind.css";
         this.shadowRoot.innerHTML = `
-        <div class="relative py-3 sm:max-w-xl sm:mx-auto w-full">
-        <div class="relative px-4 py-10 bg-black mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-            <div class="max-w-md mx-auto text-white">
-                <div class="mt-5">
-                    <label for="email" class="font-semibold text-sm text-gray-400 pb-1 block">E-mail</label>
-                    <input id="email" type="text"
-                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
-                    <label for="username" class="font-semibold text-sm text-gray-400 pb-1 block">Username</label>
-                    <input id="username" type="text"
-                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
-                    <label for="password" class="font-semibold text-sm text-gray-400 pb-1 block">Password</label>
-                    <input id="password" type="password"
-                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
-                    <label for="password2" class="font-semibold text-sm text-gray-400 pb-1 block">Confirm Password</label>
-                    <input id="password2" type="password"
-                        class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
-                </div>
-                <div class="mt-5">
-                    <button id="register"
-                        class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
-                        Register
-                    </button>
-                </div>
-                <div class="text-red-600">
-                    <p id="error"> </p>
-                </div>
-            </div>
-        </div>
-        </div>
-        `;
+      <div class="relative py-3 sm:max-w-xl sm:mx-auto w-full">
+          <div class="relative px-4 pt-5 pb-5 bg-black mx-8 md:mx-0 shadow rounded-2xl sm:px-8">
+
+              <div class="max-w-md mx-auto text-white">
+                  <form id="registerForm">
+                      <div class="mt-1">
+                          <label for="email" class="font-semibold text-sm text-gray-400 pb-1 block">E-mail</label>
+                          <input id="email" type="text"
+                              class="border rounded-lg px-3 py-2 mt-1 mb-1 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
+                          <p class="text-sm text-red-500 mb-2" id="emailError"></p>
+
+                          <label for="username" class="font-semibold text-sm text-gray-400 pb-1 block">Username</label>
+                          <input id="username" type="text"
+                              class="border rounded-lg px-3 py-2 mt-1 mb-1 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>                    
+                          <p class="text-sm text-red-500 mb-2" id="usernameError"></p>
+
+                          <label for="password" class="font-semibold text-sm text-gray-400 pb-1 block">Password</label>
+                          <input id="password" type="password"
+                              class="border rounded-lg px-3 py-2 mt-1 mb-1 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
+                          <p class="text-sm text-red-500 mb-2" id="passwordError"></p>
+                          
+                          <label for="password2" class="font-semibold text-sm text-gray-400 pb-1 block">Confirm Password</label>
+                          <input id="password2" type="password"
+                              class="border rounded-lg px-3 py-2 mt-1 mb-1 text-sm w-full bg-gray-700 text-white focus:border-blue-500 focus:ring-4 focus:ring-blue-500"/>
+                          <p class="text-sm text-red-500" id="password2Error"></p>
+                      </div>
+
+                      <div class="mt-5">
+                          <button id="register" type="submit"
+                              class="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+                              Register
+                          </button>
+                      </div>
+                  </form>
+                  <div class="text-red-500 text-sm mt-1">
+                      <p id="error"></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      `;
         this.shadowRoot.appendChild(style);
         this.emailInput = this.shadowRoot.querySelector("#email");
         this.userInput = this.shadowRoot.querySelector("#username");
         this.passwordInput = this.shadowRoot.querySelector("#password");
         this.password2Input = this.shadowRoot.querySelector("#password2");
         this.registerButton = this.shadowRoot.querySelector("#register");
+        this.registerForm = this.shadowRoot.querySelector("#registerForm");
         this.errorMsg = this.shadowRoot.querySelector("#error");
         this.addEventListeners();
     }
     addEventListeners() {
-        var _a;
-        (_a = this.registerButton) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (event) => __awaiter(this, void 0, void 0, function* () {
+        var _a, _b, _c, _d, _e;
+        (_a = this.registerForm) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (event) => __awaiter(this, void 0, void 0, function* () {
             var _a, _b, _c, _d;
             event.preventDefault();
             const email = ((_a = this.emailInput) === null || _a === void 0 ? void 0 : _a.value) || "";
             const user = ((_b = this.userInput) === null || _b === void 0 ? void 0 : _b.value) || "";
             const password = ((_c = this.passwordInput) === null || _c === void 0 ? void 0 : _c.value) || "";
             const password2 = ((_d = this.password2Input) === null || _d === void 0 ? void 0 : _d.value) || "";
-            if (email && user && password && password2) {
-                if (password === password2) {
-                    yield this.postData(email, user, password);
-                }
-                else {
-                    console.log(password);
-                    console.log(password2);
-                    this.errorMsg.textContent = "Password doesn't match";
-                    this.resetValues();
-                }
+            // Fields validation before sending the request
+            if (this.validateEmail() && this.validateUsername() && this.validatePasswordMatch()) {
+                yield this.postData(email, user, password);
             }
             else {
-                this.errorMsg.textContent = "All fields are required";
+                this.errorMsg.textContent = "Fix the errors first"; // main error message
             }
         }));
+        // Real time fields validation
+        (_b = this.emailInput) === null || _b === void 0 ? void 0 : _b.addEventListener("input", () => {
+            this.validateEmail();
+            if (this.debounceTimer)
+                clearTimeout(this.debounceTimer);
+            this.debounceTimer = window.setTimeout(() => {
+                this.checkEmailAvailability();
+            }, 400);
+        });
+        (_c = this.userInput) === null || _c === void 0 ? void 0 : _c.addEventListener("input", () => {
+            const isValid = this.validateUsername();
+            if (!isValid)
+                return;
+            if (this.debounceTimer)
+                clearTimeout(this.debounceTimer);
+            this.debounceTimer = window.setTimeout(() => {
+                this.checkUsernameAvailability();
+            }, 400);
+        });
+        (_d = this.passwordInput) === null || _d === void 0 ? void 0 : _d.addEventListener("input", () => {
+            this.checkPasswordStrength();
+        });
+        (_e = this.password2Input) === null || _e === void 0 ? void 0 : _e.addEventListener("input", () => {
+            this.validatePasswordMatch();
+        });
+    }
+    // email validation
+    validateEmail() {
+        var _a, _b;
+        const value = ((_a = this.emailInput) === null || _a === void 0 ? void 0 : _a.value) || "";
+        const emailError = (_b = this.shadowRoot) === null || _b === void 0 ? void 0 : _b.querySelector("#emailError");
+        const regex = /^\S+@\S+\.\S+$/;
+        if (!regex.test(value)) {
+            emailError.textContent = "Invalid email format"; // Print the error message
+            return false;
+        }
+        else {
+            emailError.textContent = "";
+            return true;
+        }
+    }
+    checkEmailAvailability() {
+        return __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            const email = ((_a = this.emailInput) === null || _a === void 0 ? void 0 : _a.value) || "";
+            const emailError = (_b = this.shadowRoot) === null || _b === void 0 ? void 0 : _b.querySelector("#emailError");
+            if (!this.validateEmail())
+                return; // If the email is not valid, do not check availability
+            try {
+                // Appel à l'API pour vérifier la disponibilité de l'email
+                const response = yield fetch(`http://localhost:8000/check-email?email=${encodeURIComponent(email)}`);
+                // Vérifier si la réponse est correcte
+                if (!response.ok) {
+                    emailError.textContent = "Server error, please try again later.";
+                    return;
+                }
+                const data = yield response.json();
+                // Si la clé "available" existe et est false, cela signifie que l'email est déjà pris
+                if (data && data.available) {
+                    emailError.textContent = "Email is already taken";
+                }
+                else {
+                    emailError.textContent = ""; // Aucun problème, email disponible
+                }
+            }
+            catch (err) {
+                // Gérer les erreurs réseau ou autres
+                console.error("Validation of email hasn't worked", err);
+                emailError.textContent = "Network error, please try again.";
+            }
+        });
+    }
+    // username validation
+    validateUsername() {
+        var _a, _b;
+        const value = ((_a = this.userInput) === null || _a === void 0 ? void 0 : _a.value) || "";
+        const usernameError = (_b = this.shadowRoot) === null || _b === void 0 ? void 0 : _b.querySelector("#usernameError");
+        // len check
+        if (value.length < 3) {
+            usernameError.textContent = "Must be at least 3 characters long";
+            return false;
+        }
+        //special characters check
+        const validUsernameRegex = /^[a-zA-Z0-9]+$/;
+        if (!validUsernameRegex.test(value)) {
+            usernameError.textContent = "Only letters and numbers";
+            return false;
+        }
+        usernameError.textContent = "";
+        return true;
+    }
+    checkUsernameAvailability() {
+        return __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            const username = ((_a = this.userInput) === null || _a === void 0 ? void 0 : _a.value) || "";
+            const usernameError = (_b = this.shadowRoot) === null || _b === void 0 ? void 0 : _b.querySelector("#usernameError");
+            if (username.length < 3)
+                return;
+            try {
+                // Appel à l'API pour vérifier la disponibilité du nom d'utilisateur
+                const response = yield fetch(`http://localhost:8000/check-username?username=${encodeURIComponent(username)}`);
+                // Vérifier si la réponse est correcte
+                if (!response.ok) {
+                    usernameError.textContent = "Server error, please try again later.";
+                    return;
+                }
+                const data = yield response.json();
+                if (data && data.available) {
+                    usernameError.textContent = "Username is already taken";
+                }
+                else {
+                    usernameError.textContent = ""; // Aucun problème, nom d'utilisateur disponible
+                }
+            }
+            catch (err) {
+                // Gérer les erreurs réseau ou autres
+                console.error("Validation of username hasn't worked", err);
+                usernameError.textContent = "Network error, please try again.";
+            }
+        });
+    }
+    // password1 validation
+    checkPasswordStrength() {
+        var _a, _b;
+        const value = ((_a = this.passwordInput) === null || _a === void 0 ? void 0 : _a.value) || "";
+        const passwordError = (_b = this.shadowRoot) === null || _b === void 0 ? void 0 : _b.querySelector("#passwordError");
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/; // Min 6 chars, 1 upper, 1 lower, 1 number
+        if (!value) {
+            passwordError.textContent = ""; // Si champ vide, pas d'erreur affichée
+            return false;
+        }
+        if (!regex.test(value)) {
+            passwordError.textContent = "Min 6 chars, 1 upper, 1 lower and 1 num";
+            return false;
+        }
+        else {
+            passwordError.textContent = "";
+            return true;
+        }
+    }
+    // password2 validation
+    validatePasswordMatch() {
+        var _a, _b, _c;
+        const password = ((_a = this.passwordInput) === null || _a === void 0 ? void 0 : _a.value) || "";
+        const password2 = ((_b = this.password2Input) === null || _b === void 0 ? void 0 : _b.value) || "";
+        const password2Error = (_c = this.shadowRoot) === null || _c === void 0 ? void 0 : _c.querySelector("#password2Error");
+        if (password !== password2) {
+            password2Error.textContent = "Passwords do not match";
+            return false;
+        }
+        password2Error.textContent = "";
+        return true;
     }
     postData(email, user, password) {
         return __awaiter(this, void 0, void 0, function* () {
-            const data = { username: user, email: email, password: password };
+            const data = { "username": user, "email": email, "password": password };
             try {
-                // Esta url sera el endponit que configure el servidor
                 const response = yield fetch("http://localhost:8000/register", {
                     method: "POST",
                     body: JSON.stringify(data),
@@ -104,11 +262,6 @@ class RegisterComponent extends HTMLElement {
                 });
                 this.response = yield response.json();
                 console.log("Se ha enviado la peticion");
-                // location.hash = "#profile"; // Cambiar la vista
-                // Aqui el backend hará las validaciones de email y password y me enviara un error
-                // en caso de que haya algun problema
-                // Si la autenticacion es valida, el backend creara un token jwt y lo guardara en las cookies
-                console.log(response);
                 if (response.ok) {
                     location.hash = "#";
                 }
@@ -120,16 +273,6 @@ class RegisterComponent extends HTMLElement {
                 console.log("error en la peticion");
             }
         });
-    }
-    resetValues() {
-        if (this.emailInput)
-            this.emailInput.value = "";
-        if (this.userInput)
-            this.userInput.value = "";
-        if (this.passwordInput)
-            this.passwordInput.value = "";
-        if (this.password2Input)
-            this.password2Input.value = "";
     }
 }
 customElements.define("pong-register", RegisterComponent);
