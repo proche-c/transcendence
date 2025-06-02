d504ad1e8ff605153c849614f4c1eadeca2298b2
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2025-06-02 15:18:55 +0200

    config

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-// Arxiu generat automàticament - No editar manualment
-module.exports = {
-  SERVER_IP: '172.16.118.59',
-};
--- frontend/app/config.js
+++
@@ -1,2 +0,0 @@
-// Arxiu generat automàticament - No editar manualment
-export const SERVER_IP = '172.16.118.59';
